18041c0289d172d46f233f10fba6d1e4e9ea3aa8
unknown <rkdxogh1987@gmail.com>
2022-01-04 12:35:01 +0900

    2022/01/04 Java 수업예제

---
+++ Study/src/ch03/df.java
@@ -0,0 +1,16 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class df {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("점수입력: ");
+		int Score = sc.nextInt();
+		
+		System.out.println("입력점수"+score);
+	}
+
+}
