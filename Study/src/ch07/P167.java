f1c1a3570f864d6ec0fc8d7c298b12cad9da61f2
unknown <rkdxogh1987@gmail.com>
2022-01-05 13:18:00 +0900

    2022/01/05 클래스 수업예제 추가

---
+++ Study/src/ch07/P167.java
@@ -0,0 +1,20 @@
+package ch07;
+
+public class Student{
+	//필드
+	String name;  //학생명
+	int grade;   //학년
+	String department; //학과
+	
+	Student(String n, )
+}
+
+public class P167 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
