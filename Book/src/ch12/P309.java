713694379b1a12e6fc193b57e35831db2cd04dc4
unknown <rkdxogh1987@gmail.com>
2022-01-28 18:21:52 +0900

    2022/01/28 교재예제

---
+++ Book/src/ch12/P309.java
@@ -0,0 +1,20 @@
+package ch12;
+
+public class P309 {
+	public static void main(String[] args) {
+		
+		//시작
+		long start = System.currentTimeMillis();
+		
+		String str = "";
+		for(int i=0 ; i<100000 ; i++) {
+			str += i;
+		}
+		
+		//끝
+		long end = System.currentTimeMillis();
+		System.out.println("실행 시간 : "+(end - start)/1000);
+		
+	}
+
+}
---
+++ Book/src/ch12/P310.java
@@ -0,0 +1,21 @@
+package ch12;
+
+public class P310 {
+	public static void main(String[] args) {
+		
+		//시작
+		long start = System.currentTimeMillis();
+		
+		
+		StringBuffer sb = new StringBuffer();
+		for (int i=0 ; i<100000 ; i++) {
+			sb.append(i);
+		}
+		
+		
+		//끝
+		long end = System.currentTimeMillis();
+		System.out.println("실행 시간 : "+(end - start)/1000);
+	}
+
+}
